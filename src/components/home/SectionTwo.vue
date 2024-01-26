95b9300413cb854a96145a67c8f111e43540696a
bethuel <bethuel2122@gmail.com>
2024-01-27 00:57:41 +0100

    verification des images

--- src/components/home/SectionTwo.vue
+++ src/components/home/SectionTwo.vue
@@ -3,22 +3,22 @@ import CardOne from '../shorts/CardOne.vue';
 
 const listCardOne = [
     {
-        "imageName":"global_world.svg",
+        "imageName":"../../assets/global_world.svg",
         "title":"Réseau Mondial",
         "description":"Profitez d'une plateforme d'emprunt d'argent unique surtout conçue pour apporter dignité et respect partout au monde.",
     },
     {
-        "imageName":"fast_time.svg",
+        "imageName":"../../assets/fast_time.svg",
         "title":"Instantané",
         "description":"Profitez d'emprunter de l'argent de façon simplifiéet ultra rapide 24/24h et 7/7j.",
     },
     {
-        "imageName":"valide_done.svg",
+        "imageName":"../../assets/valide_done.svg",
         "title":"Protection des données",
         "description":"Nous travaillons toujours plus pour vous assurer la sécurité de vos données personnelles et celle de vos transactions d'emprunts.",
     },
     {
-        "imageName":"lock.svg",
+        "imageName":"../../assets/lock.svg",
         "title":"Sécurité",
         "description":"Profitez d'une plateforme ultra sécurisée avec un criptage unique qui assure protection totale de vos données et autres.",
     },
